7286e995dcbdc581f8b03af02dbc58abbca838b1
Nocholas_violet <Nocholas_violet@163.com>
2020-06-19 20:56:04 +0800

    2.4.3.3 图形验证码接口实现

---
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,re_path
+
+from . import views
+
+# urlpatterns是被Django自动识别的路由列表变量：定义该应用的所有路由信息
+urlpatterns = [
+    # 函数视图路由语法：
+    # path('网络地址正则表达式', 函数视图名),
+    re_path(r'^image_codes/(?P<uuid>[\w-]+)/$', views.ImageCodeView.as_view()),
+
+
+]
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from django.http import HttpResponse
+from django_redis import get_redis_connection
+from libs.captcha.captcha import captcha
+
+class ImageCodeView(View):
+    '''返回图形验证码的类视图'''
+
+    def get(self, request, uuid):
+        '''
+        生成图形验证码, 保存到redis中, 另外返回图片
+        :param request:请求对象
+        :param uuid:浏览器端生成的唯一id
+        :return:一个图片
+        '''
+        # 1.调用工具类 captcha 生成图形验证码
+        text, image = captcha.generate_captcha()
+
+        # 2.链接 redis, 获取链接对象
+        redis_conn = get_redis_connection('verify_code')
+
+        # 3.利用链接对象, 保存数据到 redis, 使用 setex 函数
+        # redis_conn.setex('<key>', '<expire>', '<value>')
+        redis_conn.setex('img_%s' % uuid, 300, text)
+
+        # 4.返回(图片)
+        return HttpResponse(image, content_type='image/jpg')
