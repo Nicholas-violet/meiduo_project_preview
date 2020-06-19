6270dcb95bea7d7f4674ddee7b47fdd876f523b9
Nocholas_violet <Nocholas_violet@163.com>
2020-06-19 19:01:44 +0800

    导入前端全部文档

---
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+# urlpatterns是被Django自动识别的路由列表变量：定义该应用的所有路由信息
+urlpatterns = [
+    # 函数视图路由语法：
+    # path('网络地址正则表达式', 函数视图名),
+    path('/usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/', views.UsernameCountView.as_view())
+
+]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django import http
+from django.views import View
+from users.models import User
+
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, username):
+        '''判断用户名是否重复'''
+        # 1.查询username在数据库中的个数
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            return http.JsonResponse({'code':400,
+                                 'errmsg':'访问数据库失败'})
+
+        # 2.返回结果(json) ---> code & errmsg & count
+        return http.JsonResponse({'code': 0,
+                             'errmsg': 'ok',
+                             'count':count})
