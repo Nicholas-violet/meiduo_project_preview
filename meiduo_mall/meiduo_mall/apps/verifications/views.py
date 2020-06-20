331bf262fb5d7107db471ebd4566aeaca4353ffc
Nocholas_violet <Nocholas_violet@163.com>
2020-06-20 08:14:16 +0800

    2.4.3.2.3-45 容联云通讯避免频繁发送短信验证码，pipeline 操作 Redis 数据库

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -49,14 +49,22 @@ class SMSCodeView(View):
         :param mobile: 手机号
         :return: JSON
         """
+        # 将这句话提到前面最开始的位置:，  verify_code是redis对于图形的数据库的名字
+        redis_conn = get_redis_connection('verify_code')
+        # 进入函数后, 先获取存储在 redis 中的数据
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        # 查看数据是否存在, 如果存在, 说明60s没过, 返回
+        if send_flag:
+            return http.JsonResponse({'code': 400, 'errmsg': '发送短信过于频繁'})
+        # 如果过了, 在执行下面的代码:
+
         # 1. 接收参数
         image_code_client = reqeust.GET.get('image_code')
         uuid = reqeust.GET.get('image_code_id')
 
         # 2. 校验参数
         if not all([image_code_client, uuid]):
-            return http.JsonResponse({'code': 400,
-                                      'errmsg': '缺少必传参数'})
+            return http.JsonResponse({'code': 400, 'errmsg': '缺少必传参数'})
 
         # 3. 创建连接到redis的对象
         redis_conn = get_redis_connection('verify_code')
@@ -65,8 +73,7 @@ class SMSCodeView(View):
         image_code_server = redis_conn.get('img_%s' % uuid)
         if image_code_server is None:
             # 图形验证码过期或者不存在
-            return http.JsonResponse({'code': 400,
-                                      'errmsg': '图形验证码失效'})
+            return http.JsonResponse({'code': 400, 'errmsg': '图形验证码失效'})
 
         # 5. 删除图形验证码，避免恶意测试图形验证码
         try:
@@ -79,25 +86,33 @@ class SMSCodeView(View):
         image_code_server = image_code_server.decode()
         # 转小写后比较
         if image_code_client.lower() != image_code_server.lower():
-            return http.JsonResponse({'code': 400,
-                                      'errmsg': '输入图形验证码有误'})
+            return http.JsonResponse({'code': 400, 'errmsg': '输入图形验证码有误'})
 
         # 7. 生成短信验证码：生成6位数验证码
         sms_code = '%06d' % random.randint(0, 999999)
         logger.info(sms_code)
 
+        # 创建管道对象:
+        pl = redis_conn.pipeline()
+
         # 8. 保存短信验证码
         # 短信验证码有效期，单位：300秒
-        redis_conn.setex('sms_%s' % mobile,
-                         300,
-                         sms_code)
+        # redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        pl.setex('sms_%s' % mobile, 300, sms_code)
+
+        # 往 redis 中写入一个数据, 写入什么不重要, 时间重要
+        # 我们给写入的数据设置为60s,如果过期,则会获取不到.
+        # redis_conn.setex('send_flag_%s' % mobile, 60, 1)
+        pl.setex('send_flag_%s' % mobile, 60, 1)
+
+        # 执行管道:
+        pl.execute()
 
         # 9. 发送短信验证码
-        # 短信模板
-        CCP().send_template_sms(mobile,[sms_code, 5], 1)
+        # 短信模板,电话，验证码，有效时间，验证码1号模板，
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
 
         # 10. 响应结果
-        return http.JsonResponse({'code': 0,
-                                  'errmsg': '发送短信成功'})
+        return http.JsonResponse({'code': 0, 'errmsg': '发送短信成功'})
 
 
