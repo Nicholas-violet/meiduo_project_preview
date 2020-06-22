c8a76149605317a9b407698f9a6212c3b95fd865
Nocholas_violet <Nocholas_violet@163.com>
2020-06-22 16:20:06 +0800

    3.2.6 QQ的openid 绑定用户接口实现,

--- meiduo_mall/meiduo_mall/apps/oauth/utils.py
+++ meiduo_mall/meiduo_mall/apps/oauth/utils.py
@@ -17,3 +17,33 @@ def generate_access_token(openid):
 
     # 加密完之后, 解码返回.
     return token.decode()
+
+
+# 导入:
+from itsdangerous import BadData
+
+
+# 定义函数, 检验传入的 access_token 里面是否包含有 openid
+def check_access_token(access_token):
+    """
+    检验用户传入的 token
+    :param token: token
+    :return: openid or None
+    """
+
+    # 调用 itsdangerous 中的类, 生成对象
+    serializer = TimedJSONWebSignatureSerializer(settings.SECRET_KEY,
+                                                 expires_in=600)
+    try:
+        # 尝试使用对象的 loads 函数
+        # 对 access_token 进行反序列化( 类似于解密 )
+       # 查看是否能够获取到数据:
+       data = serializer.loads(access_token)
+
+    except BadData:
+        # 如果出错, 则说明 access_token 里面不是我们认可的.
+        # 返回 None
+        return None
+    else:
+        # 如果能够从中获取 data, 则把 data 中的 openid 返回
+        return data.get('openid')
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -41,6 +41,11 @@ import logging
 logger = logging.getLogger('django')
 from django.contrib.auth import login
 from oauth.utils import generate_access_token
+from django.db import DatabaseError
+import json, re
+from django_redis import get_redis_connection
+from oauth.utils import check_access_token
+from users.models import User
 
 class QQUserView(View):
     """用户扫码登录的回调处理"""
@@ -74,7 +79,93 @@ class QQUserView(View):
             # 返回结果
             return http.JsonResponse({'code': 400,
                                  'errmsg': 'oauth2.0认证失败, 即获取qq信息失败'})
-        pass
+        # pass
+
+
+    def post(self, request):
+        """美多商城用户绑定到openid"""
+
+        # 1.接收参数
+        dict = json.loads(request.body.decode())
+        mobile = dict.get('mobile')
+        password = dict.get('password')
+        sms_code_client = dict.get('sms_code')
+        access_token = dict.get('access_token')
+
+        # 2.校验参数
+        # 判断参数是否齐全
+        if not all([mobile, password, sms_code_client]):
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '缺少必传参数'})
+
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '请输入正确的手机号码'})
+
+        # 判断密码是否合格
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '请输入8-20位的密码'})
+
+        # 3.判断短信验证码是否一致
+        # 创建 redis 链接对象:
+        redis_conn = get_redis_connection('verify_code')
+
+        # 从 redis 中获取 sms_code 值:
+        sms_code_server = redis_conn.get('sms_%s' % mobile)
+
+        # 判断获取出来的有没有:
+        if sms_code_server is None:
+            # 如果没有, 直接返回:
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '验证码失效'})
+        # 如果有, 则进行判断:
+        if sms_code_client != sms_code_server.decode():
+            # 如果不匹配, 则直接返回:
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '输入的验证码有误'})
+
+            # 调用我们自定义的函数, 检验传入的 access_token 是否正确:
+        # 错误提示放在 sms_code_errmsg 位置
+        openid = check_access_token(access_token)
+        if not openid:
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '缺少openid'})
+        # 4.保存注册数据
+        try:
+            user = User.objects.get(mobile=mobile)
+        except Exception as e:
+            # 用户不存在,新建用户
+            user = User.objects.create_user(username=mobile,
+                                            password=password,
+                                            mobile=mobile)
+        else:
+            # 如果用户存在，检查用户密码
+            if not user.check_password(password):
+                return http.JsonResponse({'code': 400,
+                                     'errmsg': '输入的密码不正确'})
+        # 5.将用户绑定 openid
+        try:
+            OAuthQQUser.objects.create(openid=openid,
+                                       user=user)
+        except Exception as e:
+            return http.JsonResponse({'code': 400,
+                                 'errmsg': '往数据库添加数据出错'})
+        # 6.实现状态保持
+        login(request, user)
+
+        # 7.创建响应对象:
+        response = http.JsonResponse({'code': 0,
+                                 'errmsg': 'ok'})
+
+        # 8.登录时用户名写入到 cookie，有效期14天
+        response.set_cookie('username',
+                            user.username,
+                            max_age=3600 * 24 * 14)
+
+        # 9.响应
+        return response
 
 
 
