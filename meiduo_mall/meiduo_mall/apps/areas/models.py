a251d34893521f932503b53436f95955923d744d
Nocholas_violet <Nocholas_violet@163.com>
2020-06-23 18:24:37 +0800

    4.3.1 收货地址--省市区三级联动--模型类创建以及数据库导入

--- meiduo_mall/meiduo_mall/apps/areas/models.py
+++ meiduo_mall/meiduo_mall/apps/areas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Area(models.Model):
+    """
+    行政区划
+    """
+    # 创建 name 字段, 用户保存名称
+    name = models.CharField(max_length=20,
+                            verbose_name='名称')
+    # 自关联字段 parent
+    # 第一个参数是 self : parent关联自己.
+    # on_delete=models.SET_NULL:  如果省删掉了,省内其他的信息为 NULL
+    # related_name='subs': 设置之后
+    # 我们就这样调用获取市: area.area_set.all() ==> area.subs.all()
+    parent = models.ForeignKey('self',
+                               on_delete=models.SET_NULL,
+                               related_name='subs',
+                               null=True,
+                               blank=True,
+                               verbose_name='上级行政区划')
+
+    class Meta:
+        db_table = 'tb_areas'
+        verbose_name = '行政区划'
+        verbose_name_plural = '行政区划'
+
+    def __str__(self):
+        return self.name
