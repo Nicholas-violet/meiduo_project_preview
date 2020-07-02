8b8b1e78fe7f87b47bb64e2f4fe15ecb3236b379
Nocholas_violet <Nocholas_violet@163.com>
2020-07-02 18:20:26 +0800

    7.2.3 使用事务保存订单数据

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -11,7 +11,9 @@ from meiduo_mall.utils.views import LoginRequiredMixin
 from decimal import Decimal
 import json
 from .models import OrderInfo,OrderGoods
+from django.db import transaction
 
+# 结算订单
 class OrderSettlementView(LoginRequiredMixin, View):
     """结算订单"""
 
@@ -79,6 +81,8 @@ class OrderSettlementView(LoginRequiredMixin, View):
 
 from django.utils import timezone
 
+# 提交订单
+
 class OrderCommitView(View):
     """提交订单"""
 
@@ -123,54 +127,79 @@ class OrderCommitView(View):
         )
 
 
-        # 从 redis 读取购物车中被勾选的商品信息
-        redis_conn = get_redis_connection('carts')
-        item_dict = redis_conn.hgetall('carts_%s' % user.id)
-        cart_selected = redis_conn.smembers('selected_%s' % user.id)
-        carts = {}
-        for sku_id in cart_selected:
-            carts[int(sku_id)] = int(item_dict[sku_id])
-        # 获取选中的商品id:
-        sku_ids = carts.keys()
+        # 显式的开启一个事务
+        with transaction.atomic():
+            # 创建事务保存点
+            save_id = transaction.savepoint()
 
-        # 遍历购物车中被勾选的商品信息
-        for sku_id in sku_ids:
-            # 查询 SKU 信息
-            sku = SKU.objects.get(id=sku_id)
-            # 判断 SKU 库存
-            sku_count = carts[sku.id]
-            if sku_count > sku.stock:
-                return JsonResponse({'code': 400,
-                                     'errmsg': '库存不足'})
-
-            # SKU减少库存，增加销量
-            sku.stock -= sku_count
-            sku.sales += sku_count
-            sku.save()
-
-            # 修改SPU销量
-            sku.goods.sales += sku_count
-            sku.goods.save()
-
-            # 保存订单商品信息 OrderGoods（多）
-            OrderGoods.objects.create(
-                order=order,
-                sku=sku,
-                count=sku_count,
-                price=sku.price,
+            # 保存订单基本信息 OrderInfo（一）
+            order = OrderInfo.objects.create(
+                order_id=order_id,
+                user=user,
+                address=address,
+                total_count=0,
+                total_amount=Decimal('0'),
+                freight=Decimal('10.00'),
+                pay_method=pay_method,
+                status=OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM['ALIPAY'] else
+                OrderInfo.ORDER_STATUS_ENUM['UNSEND']
             )
 
-            # 保存商品订单中总价和总数量
-            order.total_count += sku_count
-            order.total_amount += (sku_count * sku.price)
+            # 从redis读取购物车中被勾选的商品信息
+            redis_conn = get_redis_connection('carts')
+            redis_cart = redis_conn.hgetall('carts_%s' % user.id)
+            selected = redis_conn.smembers('selected_%s' % user.id)
+            carts = {}
+            for sku_id in selected:
+                carts[int(sku_id)] = int(redis_cart[sku_id])
+            sku_ids = carts.keys()
 
-        # 添加邮费和保存订单信息
-        order.total_amount += order.freight
-        order.save()
+            # 遍历购物车中被勾选的商品信息
+            for sku_id in sku_ids:
+                # 查询SKU信息
+                sku = SKU.objects.get(id=sku_id)
+                # 判断SKU库存
+                sku_count = carts[sku.id]
+                if sku_count > sku.stock:
+                    # 出错就回滚
+                    transaction.savepoint_rollback(save_id)
+                    return JsonResponse({
+                        'code': 400,
+                        'errmsg': '库存不足'})
+
+                # SKU减少库存，增加销量
+                sku.stock -= sku_count
+                sku.sales += sku_count
+                sku.save()
+
+                # 修改SPU销量
+                sku.goods.sales += sku_count
+                sku.goods.save()
+
+                # 保存订单商品信息 OrderGoods（多）
+                OrderGoods.objects.create(
+                    order=order,
+                    sku=sku,
+                    count=sku_count,
+                    price=sku.price,
+                )
+
+                # 保存商品订单中总价和总数量
+                order.total_count += sku_count
+                order.total_amount += (sku_count * sku.price)
+
+            # 添加邮费和保存订单信息
+            order.total_amount += order.freight
+            order.save()
+
+            # 清除保存点
+            transaction.savepoint_commit(save_id)
 
         # 清除购物车中已结算的商品
-        redis_conn.hdel('carts_%s' % user.id, *cart_selected)
-        redis_conn.srem('selected_%s' % user.id, *cart_selected)
+        pl = redis_conn.pipeline()
+        pl.hdel('carts_%s' % user.id, *selected)
+        pl.srem('selected_%s' % user.id, *selected)
+        pl.execute()
 
         # 响应提交订单结果
         return JsonResponse({'code': 0,
